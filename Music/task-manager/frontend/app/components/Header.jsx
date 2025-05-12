1b0f327cdd78a51c8398095775e0098fc1e534da
KashishMahajan1203 <kashishmahajan878@gmail.com>
2025-05-12 20:58:33 +0530

    Deploy frontend folder to GitHub

---
+++ Music/task-manager/frontend/app/components/Header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <nav
+        className="navbar bg-dark border-bottom border-bottom-dark"
+        data-bs-theme="dark"
+      >
+        <div className="container">
+          <Link href={"/"} className="navbar-brand">Task Management</Link>
+          <Link
+            href={"/login"}
+            className="btn btn-outline-success"
+            type="submit"
+          >
+            Login
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
